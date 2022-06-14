6998137ce93c2cccd0d7ac3ae0fd1c421dc08bb4
JonathanMitchell1234 <88846650+JonathanMitchell1234@users.noreply.github.com>
2022-06-13 22:24:21 -0400

    added api url and axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import InputShortener from './InputShortener';
-import BackgroundAnimate from './BackgroundAnimate';
-import LinkResult from './LinkResult';
-import {useState} from 'react';
+import { useState } from "react";
+import "./App.css";
+import BackgroundAnimate from "./BackgroundAnimate";
+import InputShortener from "./InputShortener";
+import LinkResult from "./LinkResult";
 
 function App() {
-const [inputValue, setInputValue] = useState("");
+	const [inputValue, setInputValue] = useState("");
 
-  return (
-    <div className="container">
-      <InputShortener setInputValue={setInputValue}/>
-      <BackgroundAnimate/>
-      <LinkResult/>
-    </div>
-  );
+	return (
+		<div className="container">
+			<InputShortener setInputValue={setInputValue} />
+			<BackgroundAnimate />
+			<LinkResult inputValue={inputValue} />
+		</div>
+	);
 }
 
 export default App;
--- src/InputShortener.js
+++ src/InputShortener.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import { useState } from "react";
 
-const InputShortener = () => {
-    return (
-        <div className="inputContainer">
-        <h1>URL<span>Shortener</span></h1>
-        <div>
-            <input type="text" placeholder="Enter a URL to shorten it" />
-            <button>Shorten</button>
-        </div>
-        
-        </div>
+const InputShortener = ({ setInputValue }) => {
+	const [value, setValue] = useState("");
 
-    )
-}
+	const handleClick = () => {
+		setInputValue(value);
+		setValue("");
+	};
+
+	return (
+		<div className="inputContainer">
+			<h1>
+				URL <span>Shortener</span>
+			</h1>
+			<div>
+				<input type="text" placeholder="Paste a link to shorten it" value={value} onChange={(e) => setValue(e.target.value)} />
+				<button onClick={handleClick}>shorten</button>
+			</div>
+		</div>
+	);
+};
 
 export default InputShortener;
-        
-      
